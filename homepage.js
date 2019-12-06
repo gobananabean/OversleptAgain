507b14e5d25b158320a605b8ccb6605a57e9a6a7
gobananabean <57016874+gobananabean@users.noreply.github.com>
2019-12-06 15:34:21 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -9,3 +9,11 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
